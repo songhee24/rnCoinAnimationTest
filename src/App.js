b01ea6c0f691d68612fb4e1c5b089a15b873bf79
songhee24 <akaratel61@gmail.com>
2022-09-07 01:22:17 +0600

    simple basic layout of app

--- src/App.js
+++ src/App.js
@@ -11,17 +11,19 @@ import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
+  Text,
   useColorScheme,
   View,
 } from 'react-native'
 
 import { Colors } from 'react-native/Libraries/NewAppScreen'
+import CoinIcon from './components/icons/icons'
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.white,
   }
 
   return (
@@ -32,26 +34,39 @@ const App = () => {
           backgroundColor: isDarkMode ? Colors.black : Colors.white,
         }}
       />
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Главная</Text>
+        <View style={styles.coinInfo}>
+          <CoinIcon />
+          <Text style={styles.coinText}>120р.</Text>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
+  header: {
     marginTop: 32,
     paddingHorizontal: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+
+  headerTitle: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: '500',
   },
-  highlight: {
-    fontWeight: '700',
+
+  coinInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  coinText: {
+    fontSize: 16,
+    fontWeight: '500',
   },
 })
 
